fc1a10b99d36efbc0ef7a2ed37eaa26eea5d53ee
Connie Yu <connieyu18@yahoo.com>
2020-05-29 10:48:24 -0700

    created navbar v1

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,53 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import {  NavLink,Link } from "react-router-dom";
 
 import "./HeaderComponent.scss";
 
 export const HeaderComponent = (props) => {
+  const [isExpanded,handleToggle] = useState(false);
+
+
   return (
     <div className="HeaderComponent">
-
       <div className="header-left">
-        <Link
-          className="Links"
-          to={"/"}
-        >
+        <Link className="Links" to={"/"}>
           <img
-            src="/images/CodingDojoLogo.png" 
+            src="/images/CodingDojoLogo.png"
             className="dojo-logo"
             alt="Dojo logo"
           />
         </Link>
       </div>
-
-      <div className="header-right">
-        <div className={props.history.location.pathname === "/" ? "header-links-wrappers header-links-wrappers-bottomborder" : "header-links-wrappers"}>
-          <Link
-            className="Links"
-            to={"/"}
-          >
+      
+      <nav className={`header-right collapsed ${isExpanded? "is-expanded":""}`}>
+      <i
+        className="fa fa-bars"
+        aria-hidden="true"
+        onClick={()=>handleToggle(!isExpanded)}
+          />
+        <div
+          className={
+            props.history.location.pathname === "/"
+              ? "header-links-wrappers header-links-wrappers-bottomborder"
+              : "header-links-wrappers"
+          }
+        >
+          <NavLink activeClassName="active" className="Links" to={"/"}>
             <span className="header-links">Home</span>
-          </Link>
+          </NavLink>
         </div>
 
-        <div className={props.history.location.pathname === "/projects" ? "header-links-wrappers header-links-wrappers-bottomborder" : "header-links-wrappers"}>
-          <Link
-            className="Links"
-            to={"/projects"}
-          >
+        <div
+          className={
+            props.history.location.pathname === "/projects"
+              ? "header-links-wrappers header-links-wrappers-bottomborder"
+              : "header-links-wrappers"
+          }
+        >
+          <NavLink activeClassName="active" to={"/projects"}>
             <span className="header-links">Projects</span>
-          </Link>
+          </NavLink>
         </div>
 
-        <div className={props.history.location.pathname === "/signin" ? "header-links-wrappers header-links-wrappers-bottomborder" : "header-links-wrappers"}>
-          <Link
-            className="Links"
-            to={"/signin"}
-          >
+        <div
+          className={
+            props.history.location.pathname === "/signin"
+              ? "header-links-wrappers header-links-wrappers-bottomborder"
+              : "header-links-wrappers"
+          }
+        >
+          <NavLink activeClassName="active" to={"/signin"}>
             <span className="header-links">Sign In</span>
-          </Link>
+          </NavLink>
         </div>
-      </div>
-    </div >
+      </nav>
+
+    </div>
   );
 };
