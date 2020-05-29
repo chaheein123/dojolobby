9f39e9249842b27792a14837262bc9a67a0e0c36
Ian Cha <chaheein@gmail.com>
2020-05-28 21:27:53 -0700

    outline

---
+++ src/views/MainPage/MainMiddlePage/MainMiddlePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./MainMiddlePage.scss";
+
+export const MainMiddlePage = () => {
+  return (
+    <div className="MainMiddlePage">
+      Main Middle Page
+    </div>
+  )
+}
---
+++ src/views/MainPage/MainRightPage/MainRightPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./MainRightPage.scss";
+
+export const MainRightPage = () => {
+  return (
+    <div className="MainRightPage">
+      Main Right Page
+    </div>
+  )
+}
