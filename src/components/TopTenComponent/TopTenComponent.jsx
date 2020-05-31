93dd559dcef2d99faeae60960aae2d8224611a60
Ian Cha <chaheein@gmail.com>
2020-05-31 08:48:03 -0700

    progressing through the components on the left side

--- src/components/TopTenComponent/TopTenComponent.jsx
+++ src/components/TopTenComponent/TopTenComponent.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import {top10comments} from "../../data/dummydata";
+import {top10tags} from "../../data/dummydata";
 
 import "./TopTenComponent.scss";
 
 export const TopTenComponent = () => {
   return (
     <div className="TopTenComponent component-card">
-      <h5>Trending tags</h5>
+      <h6>Trending tags</h6>
       {
-        top10comments.map((comment,index) => (
+        top10tags.map((comment,index) => (
           <div key={index}>
             {index+1}. #{comment}
           </div>
--- src/data/dummydata.js
+++ src/data/dummydata.js
@@ -1,4 +1,4 @@
-let top10comments = [
+let top10tags = [
   "projects",
   "vision",
   "jobs",
@@ -11,8 +11,30 @@ let top10comments = [
   "python"
 ];
 
+let topPosts = [
+  {id:0, createdBy: "James Parker", title: "My road to being a Googler"},
+  {id: 4, createdBy: "John Oliver", title: "Should I finish college? Need advice"},
+  {id: 6, createdBy: "Ian Depp", title: "Java vs Python"},
+  {id: 12, createdBy: "Corey Chae", title: "Why I chose programming"},
+  {id: 19, createdBy: "Oliver Chen", title: "How I mastered Java"},
+  {id:200, createdBy: "Pete Kim", title: "Facebook internship posting is up"},
+]
+
+let topProjects = [ 
+  {place: 1, prize: 500, ownerId: 7, firstName: "Ian", lastName: "Cha"},
+  {place: 2, prize: 300, ownerId: 121, firstName: "Peter", lastName: "Parker"},
+  {place: 3, prize: 100, ownerId: 222, firstName: "Johnathan", lastName: "Jen"},
+];
+
+let topTA = [
+  {firstName: "Pete", lastName: "kim"},
+  // {}
+
+];
 
 export {
-  top10comments,
-  
+  top10tags,
+  topPosts,
+  topProjects,
+  topTA,
 };
